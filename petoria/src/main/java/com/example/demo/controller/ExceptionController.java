bd7485da1944ed27020cd63a87ee5173e050ad3a
hmmmuhhh <nutsa.davituliani@gmail.com>
2025-06-08 00:30:22 +0400

    initial commit

---
+++ petoria/src/main/java/com/example/demo/controller/ExceptionController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionController {
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+    return ResponseEntity.badRequest().body(e.getMessage());
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<String> handleException(Exception e) {
+    return ResponseEntity.internalServerError().body(e.getMessage());
+  }
+
+}
